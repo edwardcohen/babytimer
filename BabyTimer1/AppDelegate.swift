43c2eb6741bbd4ab291a37fbb370ef82b0a0e90e
Alexei Laine <alexei.laine@yandex.com>
2016-08-04 18:23:21 +0300

    Appsee integration

--- BabyTimer1/AppDelegate.swift
+++ BabyTimer1/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Appsee
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         BuddyBuildSDK.setup()
+        Appsee.start("580faca6704d44b6815c5985a3c96f62")
         
         // Override point for customization after application launch.
         return true
