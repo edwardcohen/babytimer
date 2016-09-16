85d9c5ea21489e54806aaa2c10fd51f4ad456861
Alexei Laine <alexei.laine@yandex.com>
2016-09-16 07:41:45 +0300

    #27 #26

--- BabyTimer1/SettingViewController.swift
+++ BabyTimer1/SettingViewController.swift
@@ -9,8 +9,14 @@
 import UIKit
 import CoreData
 import AVFoundation
+import StoreKit
 
-class SettingViewController: UIViewController, UIPopoverPresentationControllerDelegate {
+class SettingViewController: UIViewController, UIPopoverPresentationControllerDelegate, SKProductsRequestDelegate, SKPaymentTransactionObserver {
+    var purchasedProductIDs = [String]()
+    var productsArray = [SKProduct]()
+    var selectedProductIndex: Int!
+    var transactionInProgress = false
+    
     @IBOutlet var aboutButton: UIButton!
     @IBOutlet var playOnLaunchSwitch: UISwitch!
     @IBOutlet var showTimerSwitch: UISwitch!
@@ -31,6 +37,60 @@ class SettingViewController: UIViewController, UIPopoverPresentationControllerDe
         super.viewDidLoad()
 
         loadSetting()
+        
+        requestProductInfo()
+        
+        SKPaymentQueue.defaultQueue().addTransactionObserver(self)
+    }
+    
+    func requestProductInfo() {
+        if SKPaymentQueue.canMakePayments() {
+            let productRequest = SKProductsRequest(productIdentifiers: Set(["Settings"]))
+            
+            productRequest.delegate = self
+            productRequest.start()
+        } else {
+            print("Cannot perform In App Purchases.")
+        }
+    }
+    
+    func productsRequest(request: SKProductsRequest, didReceiveResponse response: SKProductsResponse) {
+        if response.products.count != 0 {
+            for product in response.products {
+                productsArray.append(product)
+                
+                let purchased = NSUserDefaults.standardUserDefaults().boolForKey(product.productIdentifier)
+                if purchased {
+                    purchasedProductIDs.append(product.productIdentifier)
+                }
+                
+            }
+        } else {
+            print("There are no products.")
+        }
+        
+        if response.invalidProductIdentifiers.count != 0 {
+            print(response.invalidProductIdentifiers.description)
+        }
+    }
+    
+    func paymentQueue(queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        for transaction in transactions {
+            switch transaction.transactionState {
+            case .Purchased:
+                print("Transaction completed successfully.")
+                SKPaymentQueue.defaultQueue().finishTransaction(transaction)
+                purchasedProductIDs.append(productsArray[selectedProductIndex].productIdentifier)
+                NSUserDefaults.standardUserDefaults().setBool(true, forKey: productsArray[selectedProductIndex].productIdentifier)
+                transactionInProgress = false
+            case .Failed:
+                print("Transaction Failed")
+                SKPaymentQueue.defaultQueue().finishTransaction(transaction)
+                transactionInProgress = false
+            default:
+                print(transaction.transactionState.rawValue)
+            }
+        }
     }
     
     func initAudioPlayer() {
@@ -53,6 +113,15 @@ class SettingViewController: UIViewController, UIPopoverPresentationControllerDe
         audioPlayer.numberOfLoops = -1
         audioPlayer.prepareToPlay()
         audioPlayer.volume = AVAudioSession.sharedInstance().outputVolume
+    }
+    
+    func showErrorMessage(message: String) {
+        let alertController = UIAlertController(title: "Error",
+                                                message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style:
+            UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alertController, animated: true, completion:
+            nil)
     }
     
     @IBAction func btnAbout() {
@@ -78,97 +147,170 @@ class SettingViewController: UIViewController, UIPopoverPresentationControllerDe
     }
 
     @IBAction func switchShowTimer() {
-        if let managedObjectContext = (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext {
-            setting!.showTimer = NSNumber(bool: showTimerSwitch.on)
-            do {
-                try managedObjectContext.save()
-            } catch {
-                print(error)
-                return
+        if productsArray.count==0 {
+            showTimerSwitch.on = !showTimerSwitch.on
+            showErrorMessage("In-App purchase Error")
+            return
+        }
+        
+        if transactionInProgress {
+            return
+        }
+        
+        selectedProductIndex = 0
+        
+        if purchasedProductIDs.contains(productsArray[selectedProductIndex].productIdentifier) {
+            if purchasedProductIDs.contains("Settings") {
+                if let managedObjectContext = (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext {
+                    setting!.showTimer = NSNumber(bool: showTimerSwitch.on)
+                    do {
+                        try managedObjectContext.save()
+                    } catch {
+                        print(error)
+                        return
+                    }
+                }
+                timerDefaultButton.hidden = !showTimerSwitch.on
+                timerDefaultLabel.hidden = !showTimerSwitch.on
+            } else {
+                let payment = SKPayment(product: productsArray[selectedProductIndex])
+                SKPaymentQueue.defaultQueue().addPayment(payment)
+                transactionInProgress = true
             }
         }
-        timerDefaultButton.hidden = !showTimerSwitch.on
-        timerDefaultLabel.hidden = !showTimerSwitch.on
     }
     
     @IBAction func buttonFadeTime() {
-        let fadeTimeMenu = UIAlertController(title: nil, message: "Select Fade Time", preferredStyle: .ActionSheet)
-
-        let times = Array(fadeTimes.keys.sort())
-        for time in times {
-            let timeAction = UIAlertAction(title: fadeTimes[time], style: .Default, handler: { action in
-                if let managedObjectContext = (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext {
-                    self.setting!.fadeTime = time
-                    do {
-                        try managedObjectContext.save()
-                        self.fadeTimeButton.setTitle(self.fadeTimes[self.setting!.fadeTime.integerValue], forState: UIControlState.Normal)
-                    } catch {
-                        print(error)
-                        return
-                    }
-                }
-            })
-            fadeTimeMenu.addAction(timeAction)
+        if productsArray.count==0 {
+            showErrorMessage("In-App purchase Error")
+            return
         }
-
-        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
-        fadeTimeMenu.addAction(cancelAction)
         
-        presentViewController(fadeTimeMenu, animated: true, completion: nil)
+        if transactionInProgress {
+            return
+        }
+        
+        selectedProductIndex = 0
+        
+        if purchasedProductIDs.contains(productsArray[selectedProductIndex].productIdentifier) {
+            let fadeTimeMenu = UIAlertController(title: nil, message: "Select Fade Time", preferredStyle: .ActionSheet)
+            
+            let times = Array(fadeTimes.keys.sort())
+            for time in times {
+                let timeAction = UIAlertAction(title: fadeTimes[time], style: .Default, handler: { action in
+                    if let managedObjectContext = (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext {
+                        self.setting!.fadeTime = time
+                        do {
+                            try managedObjectContext.save()
+                            self.fadeTimeButton.setTitle(self.fadeTimes[self.setting!.fadeTime.integerValue], forState: UIControlState.Normal)
+                        } catch {
+                            print(error)
+                            return
+                        }
+                    }
+                })
+                fadeTimeMenu.addAction(timeAction)
+            }
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+            fadeTimeMenu.addAction(cancelAction)
+            
+            presentViewController(fadeTimeMenu, animated: true, completion: nil)
+        } else {
+            let payment = SKPayment(product: productsArray[selectedProductIndex])
+            SKPaymentQueue.defaultQueue().addPayment(payment)
+            transactionInProgress = true
+        }
     }
-
+    
     @IBAction func buttonTimerDefault() {
-        let timerMenu = UIAlertController(title: nil, message: "Select Timer Default", preferredStyle: .ActionSheet)
-        
-        let timers = Array(timerDefaults.keys)
-        for timer in timers {
-            let timerAction = UIAlertAction(title: timerDefaults[timer], style: .Default, handler: { action in
-                if let managedObjectContext = (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext {
-                    self.setting!.timerDefault = timer
-                    do {
-                        try managedObjectContext.save()
-                        self.timerDefaultButton.setTitle(self.timerDefaults[timer], forState: UIControlState.Normal)
-                    } catch {
-                        print(error)
-                        return
-                    }
-                }
-            })
-            timerMenu.addAction(timerAction)
+        if productsArray.count==0 {
+            showErrorMessage("In-App purchase Error")
+            return
         }
         
-        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
-        timerMenu.addAction(cancelAction)
+        if transactionInProgress {
+            return
+        }
         
-        presentViewController(timerMenu, animated: true, completion: nil)
+        selectedProductIndex = 0
+        
+        if purchasedProductIDs.contains(productsArray[selectedProductIndex].productIdentifier) {
+            let timerMenu = UIAlertController(title: nil, message: "Select Timer Default", preferredStyle: .ActionSheet)
+            
+            let timers = Array(timerDefaults.keys)
+            for timer in timers {
+                let timerAction = UIAlertAction(title: timerDefaults[timer], style: .Default, handler: { action in
+                    if let managedObjectContext = (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext {
+                        self.setting!.timerDefault = timer
+                        do {
+                            try managedObjectContext.save()
+                            self.timerDefaultButton.setTitle(self.timerDefaults[timer], forState: UIControlState.Normal)
+                        } catch {
+                            print(error)
+                            return
+                        }
+                    }
+                })
+                timerMenu.addAction(timerAction)
+            }
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+            timerMenu.addAction(cancelAction)
+            
+            presentViewController(timerMenu, animated: true, completion: nil)
+        } else {
+            let payment = SKPayment(product: productsArray[selectedProductIndex])
+            SKPaymentQueue.defaultQueue().addPayment(payment)
+            transactionInProgress = true
+        }
+        
     }
     
     @IBAction func buttonSound() {
-        let soundMenu = UIAlertController(title: nil, message: "Select Noise Sound", preferredStyle: .ActionSheet)
-
-        for soundName in soundNames {
-            let soundAction = UIAlertAction(title: soundName, style: .Default, handler: { action in
-                if let managedObjectContext = (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext {
-                    self.setting!.soundName = soundName
-                    do {
-                        try managedObjectContext.save()
-                        self.soundButton.setTitle(soundName, forState: UIControlState.Normal)
-                        self.initAudioPlayer()
-                        self.audioPlayer.stop()
-                        self.audioPlayer.play()
-                    } catch {
-                        print(error)
-                        return
-                    }
-                }
-            })
-            soundMenu.addAction(soundAction)
+        if productsArray.count==0 {
+            showErrorMessage("In-App purchase Error")
+            return
         }
         
-        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
-        soundMenu.addAction(cancelAction)
+        if transactionInProgress {
+            return
+        }
         
-        presentViewController(soundMenu, animated: true, completion: nil)
+        selectedProductIndex = 0
+        
+        if purchasedProductIDs.contains(productsArray[selectedProductIndex].productIdentifier) {
+            let soundMenu = UIAlertController(title: nil, message: "Select Noise Sound", preferredStyle: .ActionSheet)
+            
+            for soundName in soundNames {
+                let soundAction = UIAlertAction(title: soundName, style: .Default, handler: { action in
+                    if let managedObjectContext = (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext {
+                        self.setting!.soundName = soundName
+                        do {
+                            try managedObjectContext.save()
+                            self.soundButton.setTitle(soundName, forState: UIControlState.Normal)
+                            self.initAudioPlayer()
+                            self.audioPlayer.stop()
+                            self.audioPlayer.play()
+                        } catch {
+                            print(error)
+                            return
+                        }
+                    }
+                })
+                soundMenu.addAction(soundAction)
+            }
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+            soundMenu.addAction(cancelAction)
+            
+            presentViewController(soundMenu, animated: true, completion: nil)
+        } else {
+            let payment = SKPayment(product: productsArray[selectedProductIndex])
+            SKPaymentQueue.defaultQueue().addPayment(payment)
+            transactionInProgress = true
+        }
+        
     }
     
     func loadSetting() {
